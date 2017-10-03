5946b74894090be2fb730d1b84470f0c8b3e520d
anna kälin <kaeliann@students.zhaw.ch>
2017-10-03 17:45:11 +0200

    errorhandling

--- web_server.js
+++ web_server.js
@@ -13,19 +13,32 @@ http.createServer(function (request, response) {
     console.log("Serving: " + filename);
     console.log("Serving: " + errfilename);
 
-
-
-    fs.readFile(filename, 'binary', function (err, file) {
+    fs.stat(filename, function (err, stats) {
         if (err) {
             fs.readFile(errfilename, 'binary', function (err, file) {
                 response.writeHead(404);
                 response.write(file, 'binary');
                 response.end();
             })
-        } else {
-            response.writeHead(200);
-            response.write(file, 'binary');
-            response.end();
+        } else if (stats.isDirectory()) {
+            filename = path.join(process.cwd(), "index.html");
+            console.log("Serving: " + filename);
         }
+
+
+
+        fs.readFile(filename, 'binary', function (err, file) {
+            if (err) {
+                fs.readFile(errfilename, 'binary', function (err, file) {
+                    response.writeHead(404);
+                    response.write(file, 'binary');
+                    response.end();
+                })
+            } else {
+                response.writeHead(200);
+                response.write(file, 'binary');
+                response.end();
+            }
+        })
     })
 }).listen(8080)
